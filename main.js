64cc8c995af3d51ceb00b013a1bbbba85319879d
Slavica <zsmiha@netzero.net>
2015-02-15 22:04:12 -0500

    Assignment-10 :: First Draft

---
+++ main.js
@@ -0,0 +1,13 @@
+/* example from from the class
+
+var myTemplate = _.template (
+  "<div class='product'>" +
+   "<div class='name'><%= name %></div>" +
+    "<div class='product-image'>" +
+      "<img src='<%= imgUrl %>'>" +
+   "</div>" +
+   "<div class='add-to-cart'>" +
+     "<a href='<%= href %>'>Add to cart</a>" +
+   "</div>" +
+  "</div>"
+); */
